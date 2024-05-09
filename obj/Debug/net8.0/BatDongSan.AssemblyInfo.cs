68eb8cbd9b4433cd4756ee9956918167519d2027
quochuyvo13 <quochuyvo13@gmail.com>
2024-05-09 15:44:51 +0700

    code moi nhat cua a huy

--- obj/Debug/net8.0/BatDongSan.AssemblyInfo.cs
+++ obj/Debug/net8.0/BatDongSan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+819f3f345907ebb8b51bb16e6df7802eb7eb88a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
