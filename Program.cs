4b10d7218f954ce55d8287bdf7d3b3d4bad19786
Tuan17tuiii <tn0186784@gmail.com>
2024-05-09 15:53:15 +0700

    tes2333

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors();
-
+//asdsadasdsa
+//ssdfsdf
+//sd
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+//ssdfsdf
+//sd
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
-}
+}//ssdfsdf
+ //sd
 
 app.UseHttpsRedirection();
 
