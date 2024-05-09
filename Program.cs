66f578c2ae04270f715a1896332437126d6f7e45
Tuan17tuiii <tn0186784@gmail.com>
2024-05-09 15:25:08 +0700

    test1

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddCors();
+//ahaa
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
