05905af7c4da7be5e21834835c070d9f303fbfaf
quochuyvo13 <quochuyvo13@gmail.com>
2024-05-09 15:23:21 +0700

    ahihi

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
