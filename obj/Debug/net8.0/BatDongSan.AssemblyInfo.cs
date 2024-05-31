ae114e0b860c65a96a10d05deebae3009e7af693
QuocDat2201 <hoquocdat220104@gmail.com>
2024-05-31 16:16:07 +0700

    abc

--- obj/Debug/net8.0/BatDongSan.AssemblyInfo.cs
+++ obj/Debug/net8.0/BatDongSan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac91183b729130e563c07bf45ac85cddbdd1eca3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed0764388195f7250ecf5b0967b2331f38c21da")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
