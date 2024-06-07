21461d30a5318a842a789820e815bee3b3586d51
Tuan17tuiii <tn0186784@gmail.com>
2024-06-07 23:31:57 +0700

    inforfomr

--- Services/RealestateService.cs
+++ Services/RealestateService.cs
@@ -5,12 +5,13 @@ namespace BatDongSan.Services
     public interface RealestateService
     {
         public dynamic findAll();
+
+		public dynamic search(string key);
+        public dynamic searchfilter(string? key, string? address,string? pricemin,string? pricemax, string? areamin, string? areamax);
 		public dynamic findAll2();
 		public dynamic findById(int id);
         public dynamic findByUserSellTrue(int id);
-        public dynamic findById(int id);
 		public dynamic findByUserSell(int id);
-		public dynamic findByUserSellTrue(int id);
         public dynamic findByUserSellFalse(int id);
         public dynamic findByCityRegion(string city , string region);
         public dynamic findByType(int id);
--- obj/Debug/net8.0/BatDongSan.AssemblyInfo.cs
+++ obj/Debug/net8.0/BatDongSan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa643b155611d6bf07b6a2005446b1e45a5d767e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
