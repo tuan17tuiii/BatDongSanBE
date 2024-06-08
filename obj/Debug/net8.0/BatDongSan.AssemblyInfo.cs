ce380942cc37f75823d41c53c857ee2fe9fada64
Tuan17tuiii <tn0186784@gmail.com>
2024-06-08 17:10:20 +0700

    inforfomr1

--- obj/Debug/net8.0/BatDongSan.AssemblyInfo.cs
+++ obj/Debug/net8.0/BatDongSan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b3227b56d6d7998535024507ff17d3185b23c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatDongSan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
